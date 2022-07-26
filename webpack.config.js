c48d551b039742fcd18e040b76a00f9db9d5438c
VeronicaSotoF <sotofierroveronicadev@gmail.com>
2022-07-26 10:52:30 -0500

    nuevos cambios todo app

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+
+module.exports = {
+  entry: ["@babel/polyfill", "./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "public/scripts"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env"],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "public"),
+    },
+    devMiddleware: {
+      publicPath: "/scripts/",
+    },
+    // open: true, // To open browser automatically after running code
+    hot: "only",
+  },
+  devtool: "source-map",
+};
